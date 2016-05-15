2ca8734fbceae3b29d029b21bc77642d52cee9a8
Chai Phonbopit <phonbopit@gmail.com>
2016-05-15 14:57:28 +0700

    try to add travis

--- src/factorial.js
+++ src/factorial.js
@@ -1,8 +1,15 @@
+'use strict';
+
 const recursive = (value) => {
   if (value === 1) return value;
   return value * recursive(value - 1);
 }
 
+const reduce = (value) => {
+  return value;
+}
+
 module.exports = {
-  facRecursive: recursive
+  facRecursive: recursive,
+  facReduce: reduce
 }
--- test/factorialSpec.js
+++ test/factorialSpec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { facRecursive } from '../src/factorial';
+import { facRecursive, facReduce } from '../src/factorial';
 
 describe('Factorial', () => {
   it('facRecursive(5) should return 120', () => {
@@ -8,5 +8,9 @@ describe('Factorial', () => {
 
   it('facRecursive(3) should return 6', () => {
     expect(facRecursive(3)).to.equal(6);
-  })
+  });
+
+  it('facReduce(5) should return 120', () => {
+    expect(facReduce(5)).to.equal(120);
+  });
 });
